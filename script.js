ee2aa2155012bd992f8e0e256b0c0e721a8a3e1f
HtetMe <105842411+HtetMe@users.noreply.github.com>
2026-07-29 18:37:05 +0630

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,60 @@ const revealObserver = new IntersectionObserver(
     threshold: 0.14,
     rootMargin: "0px 0px -40px 0px",
   }
+  const photos = [
+  "assets/profile.jpg",
+  "assets/portfolio-01.jpg",
+  "assets/portfolio-02.jpg",
+  "assets/portfolio-03.jpg",
+  "assets/portfolio-04.jpg"
+];
+
+let currentPhoto = 0;
+
+const image = document.getElementById("portfolioImage");
+
+function showPhoto() {
+  image.src = photos[currentPhoto];
+}
+
+function changePhoto(direction) {
+
+  currentPhoto += direction;
+
+  if (currentPhoto < 0) {
+    currentPhoto = photos.length - 1;
+  }
+
+  if (currentPhoto >= photos.length) {
+    currentPhoto = 0;
+  }
+
+  showPhoto();
+}
+
+
+// Mobile swipe
+
+let startX = 0;
+
+image.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+});
+
+
+image.addEventListener("touchend", (e) => {
+
+  let endX = e.changedTouches[0].clientX;
+
+  if (startX - endX > 50) {
+    changePhoto(1); // swipe left = next photo
+  }
+
+  if (endX - startX > 50) {
+    changePhoto(-1); // swipe right = previous photo
+  }
+
+});
 );
 
 revealItems.forEach((item) => revealObserver.observe(item));
